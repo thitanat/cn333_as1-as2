516dfd16ad17d02c526906e2df0c0c309ad6a01c
6110613210 <60341314+6110613210@users.noreply.github.com>
2021-04-02 00:31:42 +0700

    Add files via upload
    fix bug

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -1,71 +1,91 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Tham Thearawiboon on 10/2/2564 BE.
+//
+
 import Foundation
 
-struct MemoryGame<CardContent> where CardContent: Equatable {
-    var cards: Array<Card>
-    init(numberOfPairOfCards : Int , cardContentFactory: (Int) -> CardContent) {
+struct MemoryGame<CardContent: Equatable> {
+    private(set) var cards: Array<Card>
+    private(set) var Move: Int
+    var emptyIndex = 0
+    var WinStatus = ""
+    var count = 0
+        
+    init(numberOfPairsOfCard: Int, cardContentFactory: (Int) -> CardContent) {
+        Move = 0
         cards = []
-
-        for pairIndex in 0..<numberOfPairOfCards {
-            let content = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+        for Index in 0..<numberOfPairsOfCard {
+            let content = cardContentFactory(Index)
+            cards.append(Card(id: Index, content: content))
         }
-        cards.shuffle() // shuffle pair of card
-    }
-    var indexOfTheOneAndOnlyFaceUpCard: Int? {
-        get{ cards.indices.filter {cards[$0].isFaceUp }.only }
-        set{
-            for index in cards.indices {
-                cards[index].isFaceUp = index == newValue
+        cards.shuffle()
+        for i in 0..<cards.count{
+            cards[i].indexCard = i
+            if cards[i].id == 15{
+                emptyIndex = i
             }
         }
-        
     }
     
-    
-    var score : Int = 0
-    mutating func choose(card : Card){
-        if let chosenIndex = cards.firstIndex(matching: card),
-           !cards[chosenIndex].isMatched, !cards[chosenIndex].isFaceUp {
-
-
-            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
-                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
-                        cards[chosenIndex].isMatched = true
-                        cards[potentialMatchIndex].isMatched = true
-                        score = score+2
-                        
-                } else {
-                    if cards[chosenIndex].haveFacedUp == true{
-                        score = score-1
-                    }
-                    if cards[potentialMatchIndex].haveFacedUp == true{
-                        score = score-1
-                    }
-                }
-                cards[chosenIndex].haveFacedUp = true
-                cards[potentialMatchIndex].haveFacedUp = true
-            } else {
-                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+    mutating func choose(card: Card){
+        let currentCard = card.indexCard
+        let emptyCard = emptyIndex
+        if currentCard+1==emptyCard && currentCard%4 != 3{
+            cards[currentCard] = cards[emptyCard]
+            cards[emptyCard] = card
+            cards[currentCard].indexCard = currentCard
+            cards[emptyCard].indexCard = emptyCard
+            emptyIndex = currentCard
+            Move+=1
+        }
+        else if currentCard-1==emptyCard && currentCard%4 != 0{
+            cards[currentCard] = cards[emptyCard]
+            cards[emptyCard] = card
+            cards[currentCard].indexCard = currentCard
+            cards[emptyCard].indexCard = emptyCard
+            emptyIndex = currentCard
+            Move+=1
+        }
+        else if currentCard-4==emptyCard{
+            cards[currentCard] = cards[emptyCard]
+            cards[emptyCard] = card
+            cards[currentCard].indexCard = currentCard
+            cards[emptyCard].indexCard = emptyCard
+            emptyIndex = currentCard
+            Move+=1
+        }
+        else if currentCard+4==emptyCard{
+            cards[currentCard] = cards[emptyCard]
+            cards[emptyCard] = card
+            cards[currentCard].indexCard = currentCard
+            cards[emptyCard].indexCard = emptyCard
+            emptyIndex = currentCard
+            Move+=1
+        }
+        
+        for i in 0..<cards.count{
+            if cards[i].id == cards[i].indexCard{
+                count+=1
             }
-            cards[chosenIndex].isFaceUp = true
-            
+            else{
+                break
+            }
+        }
+        if count == 16 {
+            WinStatus = "Win"
         }
     }
-        
-        
-        
-        
-        
-        
-        struct Card : Identifiable {
-        var id = UUID()
-        var isFaceUp = false
-        var haveFacedUp = false
-        var isMatched = false
-        var content : CardContent
+    
+    struct Card: Identifiable {
+        var id : Int
+        var indexCard = 0
+        var content: CardContent
     }
     
-    
-    
+    func win(card : Card){
+        
+    }
 }
